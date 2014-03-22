c8d3b88a31e27ba100850b3afcbf4081eb5b7422
aherstein <adam.herstein@gmail.com>
2014-03-22 10:18:41 -0500

    Fixed a bug where link posts were showing up as 0 for the job posting body.

--- app/views/search/sections/result-detail.blade.php
+++ app/views/search/sections/result-detail.blade.php
@@ -6,7 +6,11 @@
         <a id="back"></a>
         <h2>{{$selectedJobPosting->title}}</h2><span class="time">{{$selectedJobPosting->created_time}}</span>
         <div id="post-bar">submitted by: <a href="http://reddit.com/u/{{$selectedJobPosting->author}}">{{$selectedJobPosting->author}}</a>  in reddit.com/r/{{$selectedJobPosting->subreddit->title}}</div>
+        @if ($selectedJobPosting->is_self)
         <p>{{htmlspecialchars_decode($selectedJobPosting->selftext_html)}}</p>
+        @else
+        <p>Job posting is a link to {{$selectedJobPosting->domain}}.</p>
+        @endif
     </div>
     <a href="http://reddit.com{{$selectedJobPosting->permalink}}" id="post-text-link" target="_blank">View Post on
         Reddit</a>
