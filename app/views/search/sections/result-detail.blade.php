1237cb62d38eef2fe1682e3d1fad43f4afe69f5e
aherstein <adam.herstein@gmail.com>
2014-04-12 23:01:02 -0500

    Changed subreddit title to just name of subreddit and added a link. (BitBucket Issue #4)

--- app/views/search/sections/result-detail.blade.php
+++ app/views/search/sections/result-detail.blade.php
@@ -5,7 +5,10 @@
     <div id="post-text">
         <a id="back"></a>
         <h2>{{$selectedJobPosting->title}}</h2><span class="time">{{$selectedJobPosting->created_time}}</span>
-        <div id="post-bar">submitted by: <a href="http://reddit.com/u/{{$selectedJobPosting->author}}">{{$selectedJobPosting->author}}</a>  in reddit.com/r/{{$selectedJobPosting->subreddit->title}}</div>
+
+        <div id="post-bar">submitted by: <a href="http://reddit.com/u/{{$selectedJobPosting->author}}">{{$selectedJobPosting->author}}</a>
+            in <a href="http://reddit.com/r/{{$selectedJobPosting->subreddit->title}}" target="_blank">{{$selectedJobPosting->subreddit->title}}</a>
+        </div>
         @if ($selectedJobPosting->is_self)
         <p>{{htmlspecialchars_decode($selectedJobPosting->selftext_html)}}</p>
         @else
