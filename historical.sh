2dcf6c6d68ec0b89483e726618108fa7d12c231e
aherstein <adam.herstein@gmail.com>
2014-04-12 21:01:29 -0500

    Updated scripts to pass in server name.

--- historical.sh
+++ historical.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
+server="107.170.50.188"
 subreddits=$(cat jobslist.txt)
 for subreddit in $subreddits
 do
-    curl -XPUT --user aherstein@gmail.com:nintendo  http://dev.karmajobs.net/api/historical/$subreddit
+    curl -XPUT --user aherstein@gmail.com:nintendo  http://$server/api/historical/$subreddit
     echo -e "\n"
 done
