790e57eec1f271dd1d9347ca87d76d13301a3651
aherstein <adam.herstein@gmail.com>
2014-04-28 22:21:07 -0500

    Added about page HTML and CSS.

--- app/views/page/about.blade.php
+++ app/views/page/about.blade.php
@@ -1,3 +1,32 @@
 <div id="page" class="main-column">
-	About
+	<h1>About Us</h1>
+
+	<div class="arrows"></div>
+	<hr>
+	<h2>Who's working on this project anyways?</h2>
+
+	<div class="about">
+		<h3>Daniel Allen (solidwhetstone) <em>CEO</em></h3>
+
+		<p class="dan"><a href="http://www.danieldavidallen.com" target="_blank">Dan</a> is the guy behind the design/ux and also the idea for KarmaJobs. He also provided design/ux
+			for the big 4.0 release of <a href="http://www.redditenhancementsuite.com" target="_blank">reddit enhancement suite</a>. He’s a seasoned UX designer in his own right,
+			and <a href="http://soundcloud.com/danieldavidallen">a musician on the side</a>. This site was Dan’s idea that has been in the works since 2012. Dan likes <a
+				href="http://www.reddit.com/u/solidwhetstone" target="_blank">redditing</a>, gaming, travel, and making music.</p>
+
+		<h3>Adam Herstein <em>Lead Developer</em></h3>
+
+		<p class="adam">Adam is our lead dev and the guy who wrote the code that makes KarmaJobs work. No one on the planet knows The Big Lebowski better than Adam- so don’t even
+			try to challenge his Lebowki quote knowledge. He’s also a beer master.</p>
+
+		<h3>Frank Scarpelli <em>President</em></h3>
+
+		<p class="frank"><a href="http://www.linkedin.com/in/frankscarpelli" target="_blank">Frank</a> is the business guy. He’s been involved in the world of recruiting for over 2
+			decades. He’s also got a ton of business experience. He’s sort of our recruiting industry guru that we look to help us make good business decisions that won’t
+			compromise our vision. </p>
+	</div>
+
+
+	<p class="slug"><strong>Want Updates? Want to make feature suggestions? Want to point out a bug?</strong>
+		Go subscribe to <a href="http://www.reddit.com/r/karmajobs" target="_blank">/r/karmajobs on reddit</a> and create a post (or contact us). We talk to our community every
+		day! </p>
 </div>
