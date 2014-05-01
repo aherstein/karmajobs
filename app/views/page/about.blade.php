fe942a2fb530170b06db9f542ea12f00b95b5439
Adam Herstein <adam.herstein@gmail.com>
2014-05-01 15:02:42 +0000

    Removed titles from about page.

--- app/views/page/about.blade.php
+++ app/views/page/about.blade.php
@@ -6,7 +6,7 @@
 	<h2>Who's working on this project anyways?</h2>
 
 	<div class="about">
-		<h3>Daniel Allen (solidwhetstone) <em>CEO</em></h3>
+		<h3>Daniel Allen (solidwhetstone) {{--<em>CEO</em>--}}</h3>
 
 		<p class="dan"><a href="http://www.danieldavidallen.com" target="_blank">Dan</a> is the guy behind the design/ux and also the idea for KarmaJobs. He also provided design/ux
 			for the big 4.0 release of <a href="http://www.redditenhancementsuite.com" target="_blank">reddit enhancement suite</a>. He’s a seasoned UX designer in his own right,
@@ -14,13 +14,13 @@
 			likes <a
 				href="http://www.reddit.com/u/solidwhetstone" target="_blank">redditing</a>, gaming, travel, and making music.</p>
 
-		<h3>Adam Herstein <em>Lead Developer</em></h3>
+		<h3>Adam Herstein {{--<em>Lead Developer</em>--}}</h3>
 
 		<p class="adam"><a href="https://www.linkedin.com/pub/adam-herstein/14/444/5" target="_blank">Adam</a> is our lead dev and the guy who wrote the code that makes KarmaJobs
 			work. No one on the planet knows The Big Lebowski better than Adam- so don’t even
 			try to challenge his Lebowki quote knowledge. He’s also a beer master.</p>
 
-		<h3>Frank Scarpelli <em>President</em></h3>
+		<h3>Frank Scarpelli {{--<em>President</em>--}}</h3>
 
 		<p class="frank"><a href="https://www.linkedin.com/in/frankscarpelli" target="_blank">Frank</a> is the business guy and industry expert. He is a recognized thought leader
 			in the world of careers and recruiting, possessing a ton of technology and business experience spanning over 20 years. Frank serves as our recruiting guru and guiding
