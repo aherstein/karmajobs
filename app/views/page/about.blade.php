e4def199d967a719c6196d5c04a4e56e7c1bf984
aherstein <adam.herstein@gmail.com>
2014-04-30 20:39:39 -0500

    Updated about page with updated Frank bio and link to Adam's LinkedIn. (KARMAJOBS-37)

--- app/views/page/about.blade.php
+++ app/views/page/about.blade.php
@@ -10,19 +10,21 @@
 
 		<p class="dan"><a href="http://www.danieldavidallen.com" target="_blank">Dan</a> is the guy behind the design/ux and also the idea for KarmaJobs. He also provided design/ux
 			for the big 4.0 release of <a href="http://www.redditenhancementsuite.com" target="_blank">reddit enhancement suite</a>. He’s a seasoned UX designer in his own right,
-			and <a href="http://soundcloud.com/danieldavidallen">a musician on the side</a>. This site was Dan’s idea that has been in the works since 2012. Dan likes <a
+			and <a href="http://soundcloud.com/danieldavidallen" target="_blank">a musician on the side</a>. This site was Dan’s idea that has been in the works since 2012. Dan
+			likes <a
 				href="http://www.reddit.com/u/solidwhetstone" target="_blank">redditing</a>, gaming, travel, and making music.</p>
 
 		<h3>Adam Herstein <em>Lead Developer</em></h3>
 
-		<p class="adam">Adam is our lead dev and the guy who wrote the code that makes KarmaJobs work. No one on the planet knows The Big Lebowski better than Adam- so don’t even
+		<p class="adam"><a href="https://www.linkedin.com/pub/adam-herstein/14/444/5" target="_blank">Adam</a> is our lead dev and the guy who wrote the code that makes KarmaJobs
+			work. No one on the planet knows The Big Lebowski better than Adam- so don’t even
 			try to challenge his Lebowki quote knowledge. He’s also a beer master.</p>
 
 		<h3>Frank Scarpelli <em>President</em></h3>
 
-		<p class="frank"><a href="http://www.linkedin.com/in/frankscarpelli" target="_blank">Frank</a> is the business guy. He’s been involved in the world of recruiting for over 2
-			decades. He’s also got a ton of business experience. He’s sort of our recruiting industry guru that we look to help us make good business decisions that won’t
-			compromise our vision. </p>
+		<p class="frank"><a href="https://www.linkedin.com/in/frankscarpelli" target="_blank">Frank</a> is the business guy and industry expert. He is a recognized thought leader
+			in the world of careers and recruiting, possessing a ton of technology and business experience spanning over 20 years. Frank serves as our recruiting guru and guiding
+			light, ensuring we make solid business decisions without compromising the KarmaJobs mission.</p>
 	</div>
 
 
