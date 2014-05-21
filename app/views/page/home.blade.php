94b8353e1ee96e96cf0a2b387c8a6c9b0d45aeb3
Adam Herstein <adam.herstein@gmail.com>
2014-05-21 15:16:41 +0000

    Changed links to relative path. (KARMAJOBS-40)

--- app/views/page/home.blade.php
+++ app/views/page/home.blade.php
@@ -6,13 +6,13 @@
 	<h2>Community driven job search powered by reddit. <em>Get started to the left!</em></h2>
 
 
-	<div class="home-slug forhire"><h1><a href="http://karmajobs.net/search/all/jobs/everywhere">Job Seekers</a></h1>
+	<div class="home-slug forhire"><h1><a href="/search/all/jobs/everywhere">Job Seekers</a></h1>
 
 		<p>Community Job Search is here. Search through thousands of brand new jobs posted across the reddit community- and leave public comments before applying! Get clarification
 			on compensation, location, work conditions and more all before ever sending your resume.</p>
 	</div>
 
-	<div class="home-slug hiring"><h1><a href="http://karmajobs.net/search/all/job-seekers/everywhere">Talent Seekers</a></h1>
+	<div class="home-slug hiring"><h1><a href="/search/all/job-seekers/everywhere">Talent Seekers</a></h1>
 
 		<p>Looking for a high quality talent pool that doesn’t have any barriers to entry? Look no further than KarmaJobs. There are thousands of community members from tech,
 			service, science, engineering and more using reddit every day to find work. KarmaJobs helps you tap into that pool of candidates and prequalify them before they ever
@@ -20,21 +20,21 @@
 	</div>
 
 	<div class="home-slug researchers clear">
-		<h1><a href="http://karmajobs.net/search/all/discussion/everywhere">Researchers</a></h1>
+		<h1><a href="/search/all/discussion/everywhere">Researchers</a></h1>
 
 		<p>Want to learn from the collective wisdom of the reddit jobs community? Need a way to find out what trends are developing in your field of study? Search through thousands
 			of job related posts or ask a question yourself! It’s all free. </p>
 	</div>
 
 	<div class="home-slug students">
-		<h1><a href="http://karmajobs.net/search/all/internships/everywhere">Students</a></h1>
+		<h1><a href="/search/all/internships/everywhere">Students</a></h1>
 
 		<p>Getting real world experience is a crucial first step to kickstarting your career. That means it’s time to find an internship. But where to turn? KarmaJobs makes it easy
 			to find an internship that will knock your future into high gear!</p>
 	</div>
 
 	<div class="home-slug crypto-hoarders clear">
-		<h1><a href="http://karmajobs.net/search/all/crypto-jobs/everywhere">Crypto Hoarders</a></h1>
+		<h1><a href="/search/all/crypto-jobs/everywhere">Crypto Hoarders</a></h1>
 
 		<p>Prefer to work for cryptocurrencies? Or to hire someone for crypto? KarmaJobs has you covered. Just search for crypto jobs. This is the future! THE FUTURE I TELLS YA.
 			When do we get hoverboards and Jaws 3D?</p>
